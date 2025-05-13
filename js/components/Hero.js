75b55ecd0569c450d83d38043c51c8080403d860
Cain2882 <antonytcain@gmail.com>
2025-05-13 12:08:39 +0530

    Update Hero.js

--- js/components/Hero.js
+++ js/components/Hero.js
@@ -21,7 +21,7 @@ function Hero() {
               </button>
 
               <a
-                href="public/Cain Antony Resume.pdf"
+                href="/Cain Antony Resume.pdf"
                 download="Cain Antony Resume.pdf"
                 className="px-6 py-3 bg-transparent border-2 border-serene-300 text-serene-700 rounded-lg hover:bg-serene-50 transition-all flex items-center justify-center"
               >
