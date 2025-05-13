74c2336e27805323284b805dc28eb75f696e3500
Cain2882 <antonytcain@gmail.com>
2025-05-13 15:00:22 +0530

    Update About.js

--- js/components/About.js
+++ js/components/About.js
@@ -75,7 +75,7 @@ function About() {
             </div>
 
             <a
-              href="public/Cain Antony Resume.pdf"
+              href="C:/Users/Cain Antony/SCM/public/Cain Antony Resume.pdf"
               download="Cain Antony Resume.pdf"
               className="px-6 py-3 bg-serene-500 text-white rounded-lg hover:bg-serene-600 transition-all flex items-center justify-center"
             >
